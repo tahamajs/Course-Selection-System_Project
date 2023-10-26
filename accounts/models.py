98c6502d25ddaecb3a30354a0616a0c89a18e974
RevEngine3r <91720115+RevEngine3r@users.noreply.github.com>
2023-10-26 15:24:05 +0330

    feat: User model

--- accounts/models.py
+++ accounts/models.py
@@ -13,5 +13,36 @@ class User(General):
     avatar = models.ImageField(upload_to=lambda instance, filename: f'user_{instance.user.id}/{filename}')
     phone_number = models.CharField()
     national_code = models.CharField()
-    gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')))
+    gender = models.CharField(max_length=1, choices=(('M', 'مرد'), ('F', 'زن')))
     birth_date = jmodels.jDateField()
+
+
+class Professor(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name='professor_user')
+    college = models.OneToOneField(to='', on_delete=models.CASCADE, related_name='professor_college')
+    field_of_study = models.ForeignKey(to='', on_delete=models.CASCADE, related_name='professor_field_of_study')
+    expertise = models.ForeignKey(to='', on_delete=models.CASCADE, related_name='professor_expertise')
+    degree = models.ForeignKey(to='', on_delete=models.CASCADE, related_name='professor_degree')
+
+
+class ITAdmin(models.Model):
+    user = models.OneToOneField(User, on_delete=models)
+
+
+class EducationalDeputy(models.Model):
+    user = models.OneToOneField(User, on_delete=models)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models)
+    entry_year = jmodels.jDateField()
+    entry_term = models.IntegerField(max_length=1, choices=((1, 'نیمه اول'), (2, 'نیمه دوم')))
+    gpa = models.DecimalField(max_digits=5, decimal_places=3)  # معدل => grade point average
+    college = models.ForeignKey(to='', on_delete=models.CASCADE, related_name='student_college')
+    field_of_study = models.ForeignKey(to='', on_delete=models.CASCADE, related_name='student_field_of_study')
+    courses_passed = models.ManyToManyField(to='', on_delete=models.CASCADE, related_name='student_courses_passed')
+    courses_passing = models.ManyToManyField(to='', on_delete=models.CASCADE, related_name='student_courses_passing')
+    supervisor = models.ForeignKey(to=Professor, on_delete=models.CASCADE, related_name='student_supervisor')
+    military_service_status = models.CharField(
+        choices=(('SBJ', 'مشمول'), ('MEE', 'معافیت تحصیلی'), ('MES', 'پایان خدمت')))
+    years_of_education = models.IntegerField()
