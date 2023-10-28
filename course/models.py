2f8a5f6519469d93557a020d830ffd101e454d25
taha <taha.maj4@gmail.com>
2023-10-28 11:14:33 +0330

    add ForeignKey models in models.py

--- course/models.py
+++ course/models.py
@@ -4,7 +4,7 @@ from django_jalali.db import models as jmodels
 # Create your models here.
 class Course(models.Model):
      name = models.CharField(max_length=50)
-     faculty = models.ForeignKey() # TODO: add faculty model
+     faculty = models.ForeignKey(to='college.Faculty', on_delete=models.CASCADE , related_name='faculty_course')
      prerequisite = models.ManyToManyField('self', blank=True)
      corequisite = models.ManyToManyField('self', blank=True)
      credits = models.IntegerField()
@@ -19,10 +19,10 @@ class Course(models.Model):
 
 class TermCourse(models.Model):
     course = models.ForeignKey("Course", on_delete=models.CASCADE , related_name='term_course')
-    term = models.ForeignKey() # TODO: add term model
+    term = models.ForeignKey(to='college.Term', on_delete=models.CASCADE , related_name='term_course')
     exam_date_time = jmodels.jDateTimeField()
     exam_venue = models.CharField(max_length=50)
-    profossor = models.ForeignKey() # TODO: add Profossor model
+    profossor = models.ForeignKey(to='accounts.Professor', on_delete=models.CASCADE , related_name='term_course')
     capacity = models.IntegerField()
     time = models.CharField(max_length=50)
 
@@ -34,7 +34,7 @@ class StrudentCourse(models.Model):
     course = models.ForeignKey("Course", on_delete=models.CASCADE , related_name='student_course')
     course_state = models.CharField(max_length=50 , choices=[ ('passed', 'قبول'), ('failed', 'مردود')])
     grade = models.IntegerField()
-    term = models.ForeignKey() # TODO: add term model
+    term = models.ForeignKey(to='college.Term', on_delete=models.CASCADE , related_name='student_course')
 
     def __str__(self):
         return self.course.name + ' - ' + self.term.name
