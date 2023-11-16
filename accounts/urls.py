0b6f88f8217b260ee0dc460276ae0ea94412dcfb
fatemeizadii <fatemeizadi26@ut.ac.ir>
2023-11-16 19:56:36 +0330

    ok

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from accounts.views.educational_deputy import EducationalDeputyViewSet
 from .views import *
 from rest_framework_simplejwt import views as jwt_views
+from rest_framework.routers import DefaultRouter
 
 app_name = 'accounts'
+
+router = DefaultRouter 
+router.register(r'admin/educationaldeputies', EducationalDeputyViewSet, basename='educationaldeputy')
+
 
 urlpatterns = [
     path('login/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -10,6 +16,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('change-password-request/', ChangePasswordRequestView.as_view(), name='change_password_request'),
     path('change-password-action/<uidb64>/<token>/', ChangePasswordActionView.as_view(), name='change_password_action')
-]
+] + router.urls
 
 
