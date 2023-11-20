895f889fb4742b04d7d6cbe6b12545e99b9ef492
taha <taha.maj4@gmail.com>
2023-11-20 14:30:14 +0330

    add tests college_faculty_model_api and course_term_course_model_api

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('course/', include('course.urls')),
     path('apply/', include('apply.urls')),
-    # path('',include('apply.urls'))
+    path('college/', include('college.urls')),
 ]
