81f2c6a36c3527484e572099c0eb682e67c2bf9c
fatemeizadii <fatemeizadi26@ut.ac.ir>
2023-11-22 01:37:52 +0330

    ok

--- college/urls.py
+++ college/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework import routers
 from rest_framework.routers import DefaultRouter
 from .views.faculty import FacultyViewSet
 from .views.term import TermViewSet
