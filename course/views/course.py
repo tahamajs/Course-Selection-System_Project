2eca55f5eedfbd38d5107adb64ad5ac6d549c2f7
fatemeizadii <fatemeizadi26@ut.ac.ir>
2023-11-22 13:44:22 +0330

    ok

--- course/views/course.py
+++ course/views/course.py
@@ -1,9 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-
 from course.models import Course
 from course.serializers import CourseSerializer
-
 from accounts.permissions import IsITAdmin
 from course.permissions import IsEducationalDeputyOfCourseFaculty
 
